29564e91a94df39de421561599a1b9285764f9aa
OzerOzturk <ozerozturk1636@gmail.com>
2021-05-13 19:42:40 +0300

    about page, contact page adjusted with their style. google map, form included.

---
+++ app.js
@@ -0,0 +1,14 @@
+
+/* --navigation bar-- */
+
+const navbar = document.querySelector('.navigation-bar');
+window.onscroll = () => {
+    if (window.scrollY > 300) {
+        navbar.classList.add('nav-active');
+        navbar.classList.add('items-active');
+    } else {
+        navbar.classList.remove('nav-active');
+        navbar.classList.remove('items-active');
+    }
+}; 
+
